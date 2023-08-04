94537159e18f470cc230715dbeec6321fa0e10d1
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2023-08-03 23:01:10 -0500

    Mejore estilos y aumente el tamaño maximo de los videos subidos

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Videos(models.Model):
     nombre = models.CharField(max_length=50, null=False)
-    descripcion = models.CharField(max_length=250, null=False)
+    descripcion = models.CharField(max_length=500, null=False)
     miniatura = models.FileField(upload_to='upload/photos')
     video = models.FileField(upload_to='upload/videos')
     categoria = models.CharField(max_length=15, null=False)
